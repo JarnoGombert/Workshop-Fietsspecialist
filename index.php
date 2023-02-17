023a123ebad3bc2420673a7b8969d722e620b851
JarnoGombert <jarnogombert@gmail.com>
2023-02-17 13:57:14 +0100

    commit Jarno

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+($sql = $mysqli->prepare(
+        "SELECT *,DATE_FORMAT(datum, '%W %e %M %Y') as datum1 FROM digifixx WHERE paginaurl = ? $taalquery AND status = 'actief'"
+    )) or die($mysqli->error . __LINE__);
+    $voorwaarde = $_GET["title"];
+    $sql->bind_param("s", $voorwaarde);
+$sql->execute();
+$result = $sql->get_result();
+$row = $result->fetch_assoc();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +18,17 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <header><?php include('php/header.php'); ?></header>
+    <main>
+        <?php 
+        if ($row['id'] == "1") {
+            include('php/startpagina.php');
+        } else {
+            include('php/vervolgpagina.php');
+        } ?>
+    </main>
+    <footer>
+
+    </footer>
 </body>
 </html>
