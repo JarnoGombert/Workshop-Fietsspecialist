f9c7a2af4436065bc495a9cdcee53fcba50a2d8b
JarnoGombert <jarnogombert@gmail.com>
2023-02-17 14:04:38 +0100

    footer added | Jarno

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ $row = $result->fetch_assoc();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
@@ -27,8 +28,6 @@ $row = $result->fetch_assoc();
             include('php/vervolgpagina.php');
         } ?>
     </main>
-    <footer>
-
-    </footer>
+    <footer><?php include('php/footer.php'); ?></footer>
 </body>
 </html>
