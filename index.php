b906e9476817a5d183c78166d3c37326bd8bbdf8
Jarno Gombert <j.gombert2005@gmail.com>
2023-02-28 18:15:38 +0100

    cms

--- index.php
+++ index.php
@@ -37,6 +37,10 @@ $row = $result->fetch_assoc();
 
     <link rel="shortcut icon" href="favicon.ico" />
     <link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rosario:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
     <!-- jquery files -->
     <script type="text/javascript" src="<?php echo $url; ?>/jquery/jquery-3.3.1.min.js"></script>
     <script type="text/javascript" src="<?php echo $url; ?>/jquery/digifixx.js"></script>
