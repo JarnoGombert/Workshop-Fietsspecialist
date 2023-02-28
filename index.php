a542231c211e9c0619681786f4fbd603a2db4a6b
Jarno Gombert <j.gombert2005@gmail.com>
2023-02-28 09:14:35 +0100

    icon

--- index.php
+++ index.php
@@ -30,6 +30,7 @@ $row = $result->fetch_assoc();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="favicon.ico" />
     <title><?=$row['item1'];?></title>
 </head>
 <body>
