764a7a874934b01804edd7d122ff2f394f4c6611
Jarno Gombert <j.gombert2005@gmail.com>
2023-02-28 14:22:44 +0100

    slider

--- header.php
+++ header.php
@@ -1,11 +1,17 @@
-<div class="Header-Bar">
-    <div class="Logo-Header">
-        <img src="<?=$url;?>/images/Fietsspeciaal-Bakker-Logo.png" class="image-Logo-Header" />
+<section id="header-main">
+    <div class="Header-Bar">
+        <div class="Logo-Header">
+            <img src="<?=$url;?>/images/Fietsspeciaal-Bakker-Logo.png" class="image-Logo-Header" />
+        </div>
+        <div class="Text-Header">
+            <?php include 'php/menu.php';?>
+        </div>
     </div>
-    <div class="Text-Header">
-        <?php include 'php/menu.php';?>
+</section>
+
+<section id="slider-main" class="<?php if($row['id'] != "1"){echo "slider-vervolg";};?>">
+    <div id="hero">
+        <?php// include 'php/slider.php';?>
     </div>
-</div>
-<div id="hero">
-    <?php include 'php/slider.php';?>
-</div>
+</section>
+
