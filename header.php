d31a9dfd70a02a919a379a68bd135ad49dc3dd51
Jarno Gombert <j.gombert2005@gmail.com>
2023-02-28 13:59:14 +0100

    extern menu toegevoegd

--- header.php
+++ header.php
@@ -1,11 +1,8 @@
 <div class="Header-Bar">
     <div class="Logo-Header">
-        <img src="Images/Fietsspeciaal-Bakker-Logo.png" class="image-Logo-Header" />
+        <img src="<?=$url;?>/images/Fietsspeciaal-Bakker-Logo.png" class="image-Logo-Header" />
     </div>
     <div class="Text-Header">
-        <a>Home</span>
-        <a>Producten</span>
-        <a>Over ons</span>
-        <a>Contact</span>
+        <?php include 'php/menu.php';?>
     </div>
 </div>
