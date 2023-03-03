62c9b13a6cd835e8b123de1f8ca5cddbcd2ac74b
rudemoos <m.bloemendaal210@gmail.com>
2023-03-03 12:45:17 +0100

    Pull fix van jarno

--- header.php
+++ header.php
@@ -1,11 +1,12 @@
+
 <div class="Header-Bar">
     <div class="Logo-Header">
         <img src="Images/Fietsspeciaal-Bakker-Logo.png" class="image-Logo-Header" />
     </div>
-    <div class="Text-Header">
-        <a>Home</span>
-        <a>Producten</span>
-        <a>Over ons</span>
-        <a>Contact</span>
+    <div class="container1">
+        <li class="li links"><a class="a">Home</a></li>
+        <li class="li"><a class="a">Producten</a></li>
+        <li class="li"><a class="a">Over mij</a></li>
+        <li class="li"><a class="a">Contact</a></li>
     </div>
 </div>
