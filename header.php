ed933f968edef5568f0cc768a3f47e6d65db9236
rudemoos <m.bloemendaal210@gmail.com>
2023-03-03 12:53:49 +0100

    image

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <section id="header-main">
     <div class="Header-Bar">
         <div class="Logo-Header">
-            <img src="<?=$url;?>/images/Fietsspeciaal-Bakker-Logo.png" class="image-Logo-Header" />
+            <img src="Images/Fietsspeciaal-Bakker-Logo.png" class="image-Logo-Header" />
         </div>
         <div class="Text-Header">
             <?php include 'php/menu.php';?>
@@ -11,6 +11,7 @@
 
 <section id="slider-main" class="<?php if($row['id'] != "1"){echo "slider-vervolg";};?>">
     <div id="hero">
+
     </div>
 </section>
 
