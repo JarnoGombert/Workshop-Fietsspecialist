50c5f83e11c4e284b9d7e0127f1df13e5ebecb6a
rudemoos <m.bloemendaal210@gmail.com>
2023-03-10 13:06:34 +0100

    Card add

--- startpagina.php
+++ startpagina.php
@@ -1,4 +1,14 @@
 <section class="contentHomeMain">
 
-<div class="Rectagnle-main"></div>
+<div class="Rectagnle-main">
+    <div class="card1">
+
+    </div>
+    <div class="card2">
+
+    </div>
+    <div class="card3">
+        
+    </div>
+</div>
 </section>
