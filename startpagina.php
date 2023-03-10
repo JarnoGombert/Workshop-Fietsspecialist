605d18d4b156b12b23bd65204dc6faebd5536d10
rudemoos <m.bloemendaal210@gmail.com>
2023-03-10 13:47:37 +0100

    Cards done

--- startpagina.php
+++ startpagina.php
@@ -3,13 +3,21 @@
 <div class="Rectagnle-main">
     <div class="card">
         <img src="<?=$url;?>Images/FietsImage1.png" class="ImgCard"/>
-        <div class="TxtCard">Bekijk nu onze producten</div>
+        <div class="TxtCard">
+            <p style="margin-left: 10px;">Bekijk nu </br> onze producten</p>
+        </div>
     </div>
     <div class="card">
     <img src="<?=$url;?>Images/FietsImage2.png" class="ImgCard"/>
+    <div class="TxtCard">
+            <p style="margin-left: 10px;">Lees meer over </br> Fietsspeciaal Bakker</p>
+        </div>
     </div>
     <div class="card">
     <img src="<?=$url;?>Images/FietsImage3.png" class="ImgCard"/>
+        <div class="TxtCard">
+                <p style="margin-left: 10px;">Kom kijken of </br> maak een afspraak</p>
+        </div>
     </div>
 </div>
 </section>
