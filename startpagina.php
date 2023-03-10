9453e93cdea40c5f9e8b9928668733f05ba528c1
rudemoos <m.bloemendaal210@gmail.com>
2023-03-10 13:25:17 +0100

    Rectagnele image add

--- startpagina.php
+++ startpagina.php
@@ -2,13 +2,13 @@
 
 <div class="Rectagnle-main">
     <div class="card">
-
+        <img src="<?=$url;?>Images/FietsImage1.png" class="ImgCard"/>
     </div>
     <div class="card">
-
+    <img src="<?=$url;?>Images/FietsImage2.png" class="ImgCard"/>
     </div>
     <div class="card">
-
+    <img src="<?=$url;?>Images/FietsImage3.png" class="ImgCard"/>
     </div>
 </div>
 </section>
