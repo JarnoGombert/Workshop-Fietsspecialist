4ebcc915a8de5cde45bc8e3399d2ad8077a2cbf4
rudemoos <m.bloemendaal210@gmail.com>
2023-03-10 14:09:36 +0100

    Product Cards

--- startpagina.php
+++ startpagina.php
@@ -20,4 +20,45 @@
         </div>
     </div>
 </div>
+<div class="Product-main">
+    <div class="ProductTxt">Producten</div>
+    <div class="ProductCard">
+        <div class="card">
+            <img src="<?=$url;?>Images/FietsCard.png" class="ImgCard"/>
+            <div class="TxtCard">
+                Premio EVO 5 Lite Comfort
+            </div>
+            <div class="TxtCard">
+                €3.749,00
+            </div>
+        </div>
+        <div class="card">
+            <img src="<?=$url;?>Images/FietsCard.png" class="ImgCard"/>
+            <div class="TxtCard">
+                Premio EVO 5 Lite Comfort
+            </div>
+            <div class="TxtCard">
+                €3.749,00
+            </div>
+        </div>
+        <div class="card">
+            <img src="<?=$url;?>Images/FietsCard.png" class="ImgCard"/>
+            <div class="TxtCard">
+                Premio EVO 5 Lite Comfort
+            </div>
+            <div class="TxtCard">
+                €3.749,00
+            </div>
+        </div>
+        <div class="card">
+            <img src="<?=$url;?>Images/FietsCard.png" class="ImgCard"/>
+            <div class="TxtCard">
+                Premio EVO 5 Lite Comfort
+            </div>
+            <div class="TxtCard">
+                €3.749,00
+            </div>
+        </div>
+    </div>
+</div>
 </section>
