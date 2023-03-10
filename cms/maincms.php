5086371ce850e79b5273526126b34927672334f4
JarnoGombert <jarnogombert@gmail.com>
2023-03-10 14:09:29 +0100

    cms

--- cms/maincms.php
+++ cms/maincms.php
@@ -7,8 +7,8 @@ include 'login/config.php';
 $sqluser = $mysqli->query("SELECT * FROM digifixxcms_gebruikers WHERE id = '1' ") or die($mysqli->error.__LINE__);
 $rowuser = $sqluser->fetch_assoc();
 
-if(!$mysqli) { header ('Location:../'); }
-if(login_check($mysqli) <> true) { header ('Location:../'); }
+// if(!$mysqli) { header ('Location:../'); }
+// if(login_check($mysqli) <> true) { header ('Location:../'); }
 
 $urlCMS = $url."cms";
 ?>
@@ -21,9 +21,12 @@ $urlCMS = $url."cms";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?=$urlCMS;?>/css/cmsStyle.css">
     <link rel="shortcut icon" href="<?=$url;?>favicon.ico" />
+    <!-- jquery files -->
+    <script type="text/javascript" src="<?=$url;?>jquery/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="<?=$url;?>jquery/digifixx.js"></script>
     <link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico" />
     <title>Digifixx CMS</title>
-    <style>body{background: #41485085;}</style>
+    <style>body{background: #41485085;}.open_menu{display: flex !important;}</style>
 </head>
 <body>
     <header><?php include ('php/menu.php'); ?></header>
