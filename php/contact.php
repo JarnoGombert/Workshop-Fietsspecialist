1ecf46e62b6a9b8a2a7a71164db32617e8aa0aab
rudemoos <m.bloemendaal210@gmail.com>
2023-03-16 17:49:11 +0100

    Contact page

--- php/contact.php
+++ php/contact.php
@@ -1,3 +1,62 @@
-<div id="contact">
+<div id="contact" class="contact">
+    <div class="Rectagnle-Contact">
+        <div class="Rectagnle">
+                <img src="../Images/Call-Logo.png" alt="" style=" width: 150px; height: 150px; text-align: center; margin-top: 10px">
 
+            <div class="BigTXT">
+                Bel ons<br/><br/>
+            </div>
+            <div class="SmallTXT">
+                Telefonische Ondersteuning <br/> is beschikbaar van 9 tot 5. <br/> Bel ons gerust!
+            </div>
+        </div>
+        <div class="Rectagnle">
+                <img src="../Images/Mail-Logo.png" alt="" style=" width: 150px; height: 150px; text-align: center; margin-top: 10px">
+            <div class="BigTXT">
+                E-mail ons<br/><br/>
+            </div>
+            <div class="SmallTXT">
+                Voor algemene vragen <br/> kunt u via e-mail contact <br/> met ons opnemen
+            </div>
+        </div>
+        <div class="Rectagnle">
+                    <img src="../Images/Locatie-Logo.png" alt="" style=" width: 150px; height: 150px; text-align: center; margin-top: 10px">
+                <div class="BigTXT">
+                    Bezoek ons<br/><br/>
+                </div>
+                <div class="SmallTXT">
+                    Meesterlaan 37, <br/> 7241 EJ Lochem <br/> 
+                </div>
+            </div>
+    </div>
+    <div class="Afspraak-Main">
+        <div class="Rectagnle">
+            <div>
+                <img src="" alt="">
+            </div>
+            <div>
+                <div class="AfspraakTXT">
+                    Maak een Afspraak:
+                </div>
+                <form class="form-group">
+                    <label for="name">Naam:</label>
+                    <input type="text" id="name" name="name"><br><br>
+                    
+                    <label for="date">Datum:</label>
+                    <input type="date" id="date" name="date"><br><br>
+                    
+                    <label for="time">Tijd:</label>
+                    <input type="time" id="time" name="time"><br><br>
+                    
+                    <label for="message">Message:</label>
+                    <textarea id="message" name="message" rows="5" cols="50"></textarea><br><br>
+                    
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+
+        </div>
+
+
+    </div>
 </div>
