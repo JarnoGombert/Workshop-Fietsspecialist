5fd106996d857f016ea6d641c80f5a87b3be3afc
rudemoos <m.bloemendaal210@gmail.com>
2023-03-17 13:28:41 +0100

    review

--- vervolgpagina.php
+++ vervolgpagina.php
@@ -7,6 +7,61 @@
             <article><?=$row['tekst'];?></article>
         </div>
         <?php
+        if($row['id'] == '3') {
+            ?>
+        <div class="Review">
+            <h1 style="text-align: center; margin-bottom: 50px; font-size: 40px">Reviews:</h1>
+            <div class="ReviewRow1">
+                <div class="ReviewCard">
+                    <div class="ImageWrapper mx-auto">
+                        <img src="<?=$url;?>Images/Vrouw1.png" alt="">
+                    </div>
+                    <div class="reviewTxt">
+                        Alisha de Jong
+                    </div>
+                    <div class="reviewTxt">
+                        7/10
+                    </div>
+                    <div class="reviewMessageTxt">
+                        Dit is een prachtige en efficiente website!
+                    </div>
+
+                </div>
+                <div class="ReviewCard">
+                    <div class="ImageWrapper mx-auto">
+                        <img src="<?=$url;?>Images/Vrouw1.png" alt="">
+                    </div>
+                    <div class="reviewTxt">
+                        Alisha de Jong
+                    </div>
+                    <div class="reviewTxt">
+                        7/10
+                    </div>
+                    <div class="reviewMessageTxt">
+                        Dit is een prachtige en efficiente website!
+                    </div>
+
+                </div>
+                <div class="ReviewCard">
+                    <div class="ImageWrapper mx-auto">
+                        <img src="<?=$url;?>Images/Vrouw1.png" alt="">
+                    </div>
+                    <div class="reviewTxt">
+                        Alisha de Jong
+                    </div>
+                    <div class="reviewTxt">
+                        7/10
+                    </div>
+                    <div class="reviewMessageTxt">
+                        Dit is een prachtige en efficiente website!
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+        <?php
+        }
             if($row['id'] == '4') {
                 include 'php/contact.php';
             }
