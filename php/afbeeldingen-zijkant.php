fbd1f4e9e78d5594eaaae4e18e527b6cac9d7845
JarnoGombert <jarnogombert@gmail.com>
2023-03-17 10:04:42 +0100

    vervolg

--- php/afbeeldingen-zijkant.php
+++ php/afbeeldingen-zijkant.php
@@ -1,13 +1,19 @@
 <div class="image-wrapper">
     <?php
-    $query = $mysqli->query("SELECT * FROM digifixx_images WHERE cms_id = ".$row['id']."");
-    if($query->num_rows > 0) {
-        while($row = $query->fetch_assoc()){
-            $imageURL = '../img/'.$row["file_name"];
+    $afbeeldingZijkant = $mysqli->prepare("SELECT file_name FROM digifixx_images WHERE cms_id = ?");
+    $afbeeldingZijkant->bind_param('i', $row['id']);
+    $afbeeldingZijkant->execute();
+    $afbeeldingZijkant->store_result();
+    $afbeeldingZijkant->bind_result($naamImgZijkant);
+
+
+    while($afbeeldingZijkant->fetch()){
+            if($afbeeldingZijkant->num_rows > 0) {
+            $imageURL = '../img/'.$naamImgZijkant;
         ?>
         <div class="img">
             <img src="<?php echo $imageURL; ?>" height="100%" width="100%" alt="" />
         </div>
-    <?php }
- } ?>
+        <?php }
+    } ?>
 </div>
