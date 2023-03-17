8256f86f302e1095c6054801630f2a8cd1b5d29d
JarnoGombert <jarnogombert@gmail.com>
2023-03-17 14:54:27 +0100

    cms

--- cms/php/menu.php
+++ cms/php/menu.php
@@ -7,13 +7,15 @@
             <span class="nieuw_menu"><span class="username">Nieuw</span><span class="ti-plus"></span></span>
             <div class="nieuw_opties">            
                 <a href="?page=nieuwe_pagina"><span class="menu-icon ti-files"></span>nieuwe pagina</a>
-                <a href="?page=nieuwe_gebruiker"><span class="menu-icon ti-user"></span>nieuwe gebruiker</a>
+                <?php if($UserId == '1') { ?>
+                    <a href="?page=nieuwe_gebruiker"><span class="menu-icon ti-user"></span>nieuwe gebruiker</a>
+                <?php } ?>
             </div>
         </div>
         <div id="gebruiker">
-            <span class="gebruiker_menu"><span class="menu-icon ti-user"></span><span class="username">Welkom <?php echo $rowuser['username'] ?></span><span class="ti-angle-down"></span></span>
+            <span class="gebruiker_menu"><span class="menu-icon ti-user"></span><span class="username">Welkom <?php echo $rowUser['username']; ?></span><span class="ti-angle-down"></span></span>
             <div class="gebruiker_opties">            
-                <a href="?page=gebruiker_bewerken&id=<?php echo $rowuser['id']; ?>"><span class="menu-icon ti-settings"></span>mijn account</a>
+                <a href="?page=gebruiker_bewerken&id=<?php echo $UserId; ?>"><span class="menu-icon ti-settings"></span>mijn account</a>
                 <a href="index.php?uitloggen=ja"><span class="menu-icon ti-lock"></span>uitloggen</a>
             </div>
         </div>
