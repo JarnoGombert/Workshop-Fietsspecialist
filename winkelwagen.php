6d776fb82a0c3c7caba100bb3a1208bf8fb0ba9b
rudemoos <m.bloemendaal210@gmail.com>
2023-03-17 10:37:49 +0100

    push

--- winkelwagen.php
+++ winkelwagen.php
@@ -2,5 +2,30 @@
     <div class="container mx-auto">
         <?php include 'php/breadcrumbs.php'; ?>
        <div class="title"><?=$row['item1'];?></div> 
+        <div class="winkelwagen-Main">
+            <div class="showProducts">
+                <img src="<?=$url;?>Images/FietsCard.png" class="ImgWinkelwagen"/>
+                <div class="winkelwagenTXT">
+                Premio EVO 5 Lite Comfort
+                </div>
+                <div class="winkelwagenTXT">
+                €3.749,00
+                </div>
+                <div class="winkelwagenTXT">
+                    1        
+                </div>
+                <div class="Delete">
+                    <a >
+                        <i class="fa fa-trash-o"></i>
+                    </a>
+                </div>
+            </div> 
+            <div class="winkelwagenBetalen">
+
+            </div>
+        </div>
+        
+
     </div>
+
 </section>
