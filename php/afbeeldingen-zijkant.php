891839db39dc492623e954499e8b80c36acb41d3
JarnoGombert <jarnogombert@gmail.com>
2023-03-17 09:41:13 +0100

    afbeeldingen

--- php/afbeeldingen-zijkant.php
+++ php/afbeeldingen-zijkant.php
@@ -1,11 +1,13 @@
 <div class="image-wrapper">
     <?php
     $query = $mysqli->query("SELECT * FROM digifixx_images WHERE cms_id = ".$row['id']."");
+    if($query->num_rows > 0) {
         while($row = $query->fetch_assoc()){
             $imageURL = '../img/'.$row["file_name"];
         ?>
         <div class="img">
             <img src="<?php echo $imageURL; ?>" height="100%" width="100%" alt="" />
         </div>
-    <?php } ?>
+    <?php }
+ } ?>
 </div>
