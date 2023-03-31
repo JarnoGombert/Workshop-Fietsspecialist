49bc700cc3d9289b3605ae372dfc8a2b70adec58
rudemoos <m.bloemendaal210@gmail.com>
2023-03-31 12:42:54 +0200

    creditcard tering moet pushen niet gym maar het project

--- php/betalingsmethode.php
+++ php/betalingsmethode.php
@@ -1,3 +1,16 @@
-<div>
-    test
-</div>
+  <div class="Creditcard-main">
+    <h1>Creditcard:</h1>
+    <label for="card-number">kaart nummer:</label>
+    <input type="text" id="card-number" name="card-number" placeholder="1234 5678 9012 3456" required><br/>
+
+    <label for="card-holder-name">kaart houder naam:</label>
+    <input type="text" id="card-holder-name" name="card-holder-name" placeholder="John Doe" required><br/>
+
+    <label for="expiry-date">afloop datum:</label>
+    <input type="text" id="expiry-date" name="expiry-date" placeholder="MM/YY" required><br/>
+
+    <label for="cvv">CVV:</label>
+    <input type="text" id="cvv" name="cvv" placeholder="123" required><br/>
+
+    <input type="submit" value="Betaal">
+  </div>
