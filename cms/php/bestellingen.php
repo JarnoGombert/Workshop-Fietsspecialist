9ed0886a5db8857078416cf94fe6f5a9fd06d90f
Jarno Gombert <j.gombert2005@gmail.com>
2023-04-25 18:06:32 +0200

    orders kun je bijhouden

--- cms/php/bestellingen.php
+++ cms/php/bestellingen.php
@@ -9,17 +9,20 @@ $sqlOrder->bind_result($idOrder, $productsOrder, $MethodOrder, $Total_Price_Orde
 
 <section id="bestelling">
     <div class="title">Mijn bestellingen</div>
-    <?php while($sqlOrder->fetch()){ ?>
+    <?php 
+    $orderCount = 1;
+    while($sqlOrder->fetch()){ ?>
         <div id="orderTab">
+            <h2>Order: <?=$orderCount;?></h2>
             <?php
             $producten = json_decode($productsOrder);
             for($i = 0; $i < count($producten); $i++){
                 $productID = $producten[$i];
-                $orderFietsen = $mysqli -> prepare("SELECT id, naam, model, merk, prijs, status FROM digifixx_producten WHERE id = ?") or die ($mysqli->error.__LINE__);
+                $orderFietsen = $mysqli -> prepare("SELECT id, naam, model, merk, prijs_korting, prijs, status FROM digifixx_producten WHERE id = ?") or die ($mysqli->error.__LINE__);
                 $orderFietsen->bind_param('i',$productID);
                 $orderFietsen->execute();
                 $orderFietsen->store_result();
-                $orderFietsen->bind_result($fietsID, $naamFiets, $modelFiets, $merkFiets, $prijsFiets, $statusFiets);
+                $orderFietsen->bind_result($fietsID, $naamFiets, $modelFiets, $merkFiets, $prijsKFiets, $prijsFiets, $statusFiets);
                 while($orderFietsen->fetch()){ 
                 $FietsIMG = $mysqli->query("SELECT * FROM digifixx_images WHERE product_id = ".$fietsID."");
                 $rowIMG = $FietsIMG->fetch_assoc();
@@ -31,10 +34,30 @@ $sqlOrder->bind_result($idOrder, $productsOrder, $MethodOrder, $Total_Price_Orde
                     $imageURL = '../img/noimg.jpg';
                 }
                 ?>
-                <img src="<?=$imageURL;?>" alt="">
+                <div class="bestelling-fiets">
+                    <div>item: <?=$i + 1;?></div>
+                    <img src="<?=$imageURL;?>" alt="">
+                    <div><?=$merkFiets;?><?=$modelFiets;?><?=$naamFiets;?></div>
+                    <div>
+                        <?php if($prijsKFiets != "0"){?>
+                                <small class="PrijsPproduct">€ <?=$prijsFiets;?></small><br/>
+                                <span>Prijs: € <?=$prijsKFiets;?></span>
+                        <?php } else { ?>
+                            <span>Prijs: € <?=$prijsFiets;?></span>
+                        <?php } ?>
+                    </div>
+                </div>
             <?php }
             }
             ?>
+
+            <div class="order-details">
+                <div><strong>Betaal Methode: </strong><?=ucfirst($MethodOrder);?></div>
+                <div><strong>Totale Prijs: </strong>€ <?=$Total_Price_Order;?>,-</div>
+                <div></div>
+            </div>
         </div>
-    <?php } ?>
+    <?php 
+    $orderCount++;
+    } ?>
 </section>
